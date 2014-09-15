21b76f0add96ae56fbad6edb1b097136a1ab6d64
chris sandulow <chris.sandulow@mongodb.com>
2014-09-15 12:57:44 -0400

    initial commit

---
+++ cam_slurper.py
@@ -0,0 +1,225 @@
+#!/usr/bin/env python
+
+''' Do things w/ AirBeam devices; see github repo for readme '''
+
+import yaml
+import argparse
+import pickle
+import requests
+import logging
+import sys
+import time
+import os
+from lxml import etree
+from BeautifulSoup import BeautifulSoup
+
+
+def setup_logging(args, config):
+    ''' stdout for warning/errors, logfile for everything '''
+
+    if args.debug:
+        print 'Debugging to log file'
+        level = logging.DEBUG
+    else:
+        level = logging.INFO
+
+    logging.basicConfig(filename=config['log']['file'],
+                        format=config['log']['format'],
+                        datefmt=config['log']['dateformat'],
+                        level=level)
+    console = logging.StreamHandler()
+    console.setLevel(logging.WARNING)
+    formatter = logging.Formatter(fmt=config['log']['format'],
+                                  datefmt=config['log']['dateformat'])
+    console.setFormatter(formatter)
+    logging.getLogger('requests').propagate = False
+    logging.getLogger('').addHandler(console)
+
+    return logging
+
+class CamSlurper:
+    ''' CamSlurper object to do things w/ AirBeam devices '''
+
+    def __init__(self, name, hostname, store_path):
+        self.hostname = hostname
+        self.name = name
+        self.storage_path = store_path
+
+    def _rget(self, path):
+        ''' Generic wrapper for requests get method '''
+        url = self.hostname + '/' + path
+        r = requests.get(url)
+        if r.status_code == 200:
+            return r
+        else:
+            logging.error('Error getting %s: -> %s' % (url, str(sys.exc_info())))
+
+    def info(self):
+        ''' Gets current info from recording device, returns JSON object of
+            status items I care about '''
+        r = self._rget('info')
+        doc = etree.fromstring(r.content)
+        out = { 'camera_status'   : doc.xpath('//camera/available')[0].text,
+                'record_status'   : doc.xpath('//recording/status')[0].text,
+                'record_avail'    : doc.xpath('//recording/available')[0].text,
+                'record_duration' : doc.xpath('//recording/duration')[0].text,
+                'device_name'     : doc.xpath('//name')[0].text
+              }
+        return out
+
+    def get_duration(self):
+        ''' returns duration of recording, in number of seconds '''
+        info = self.info()
+        time = info['record_duration']
+        logging.debug('Raw Duration time: %s' % time)
+        try:
+            hours, mins, secs = time.split(':')
+            duration = int(secs) + (int(mins) * 60) + (int(hours) * 3600)
+            logging.debug('Current Duration: %d' % duration)
+            return duration
+        except:
+            return 0
+
+    def start_record(self):
+        ''' Starts Recording '''
+        r = self._rget('record')
+        logging.info('Device [%s]: Recording Started' % self.name)
+        time.sleep(10) # wait required due to start time lag on some devices
+
+    def stop_record(self):
+        ''' Stops Recording '''
+        r = self._rget('stoprecord')
+        logging.info('Device [%s]: Recording Stopped' % self.name)
+
+    def start_camera(self):
+        ''' Turns on the Camera (must be turned on before we record) '''
+        r = self._rget('startcamera')
+        logging.debug('Device [%s]: Camera Turned on' % self.name)
+
+    def stop_camera(self):
+        ''' Turns off the Camera '''
+        r = self._rget('stopcamera')
+        logging.info('Device [%s]: Camera Turned off' % self.name)
+
+    def list_recordings(self):
+        ''' Returns a list of recording filenames '''
+        r = self._rget('recordings.html')
+        html = r.content
+        parsed_html = BeautifulSoup(html)
+        recordings = set()
+        for link in parsed_html.findAll('a'):
+            l = link.get('href')
+            if l.startswith('/recording/'):
+                recordings.add(l.split('/')[-1])
+        logging.debug('Current files: %s' % str(recordings))
+        return recordings
+
+    def download_recording(self, file_name):
+        url = self.hostname + '/recording/' + file_name
+        path = self.storage_path + str(time.time())[:10] + '_' + file_name
+        with open(path, 'wb') as handle:
+            response = requests.get(url, stream=True)
+
+            if not response.ok:
+                logging.error('Could not download/write file -> %s' % sys.exc_info())
+                return 'not ok'
+
+            for block in response.iter_content(1024):
+                if not block:
+                    break
+
+                handle.write(block)
+        logging.info('Downloaded file %s; saved to %s' % (file_name, path))
+        return 'ok'
+
+    def delete_recording(self, file_name):
+        ''' Deletes a recording '''
+        r = self._rget('delete/' + str(file_name))
+        logging.info('Device [%s]: "%s" deleted.' % (self.name, str(file_name)))
+
+    def get_storage_dir_size(self):
+        ''' returns the size of the storage dir (in MB) '''
+        total_size = 0
+        p = self.storage_path
+        for dirpath, dirnames, filenames in os.walk(p):
+            for f in filenames:
+                fp = os.path.join(dirpath, f)
+                total_size += os.path.getsize(fp)
+        return total_size / (1024*1024)
+
+    def remove_oldest_file(self):
+        ''' Deletes the oldest file in the storage directory, if dir size
+            is greater than specified in the config '''
+        files = os.listdir(self.storage_path)
+        filel = []
+        for f in files:
+            if f.endswith('.mov'): filel.append(f)
+        files = sorted(filel, reverse=True)
+        delfile = self.storage_path + '/' + files[-1]
+        os.remove(delfile)
+        logging.info('Deleted old file %s' % delfile)
+
+def main(args, config):
+    logging = setup_logging(config=config, args=args)
+
+    cname      = config['camera']['name']
+    hostname   = config['camera']['hostname']
+    store_path = config['camera']['store_path']
+    rotate     = config['rotate_duration']
+
+    while True:
+        cs = CamSlurper(name=cname,
+                         hostname=hostname,
+                         store_path=store_path)
+
+        # Make sure Camera is OK to record
+        info = cs.info()
+        if info['camera_status'] != 'YES': cs.start_camera()
+        if info['record_avail'] != 'YES': cs.start_camera()
+
+        # Check how long we've been recording for
+        duration = cs.get_duration()
+        # if we're not recording, start it.
+        if duration == 0:
+            logging.debug('Duration == 0')
+            cs.start_record()
+        # if we hit rotate time, rotate
+        elif duration > rotate:
+            logging.info('Rotating file')
+            cs.stop_record()
+            cs.start_record()
+
+        # check the size of files in the storage directory
+        cur_size = cs.get_storage_dir_size()
+        while cur_size > int(config['max_dir_size']):
+            logging.debug('Current storage dir size is %d' % cur_size)
+            cs.remove_oldest_file()
+            time.sleep(1)
+            cur_size = cs.get_storage_dir_size()
+
+        # See if there are any files to download
+        files = cs.list_recordings()
+        if files:
+            for f in files:
+                resp = cs.download_recording(f)
+                if resp == 'ok':
+                    cs.delete_recording(f)
+
+        time.sleep(5)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='''Python Script to ensure an AirBeam
+        device is recording and we are downloading the recordings periodically''')
+    parser.add_argument('-c', '--config', dest='config', required=True,
+                        help='(Required) YAML configuration file')
+    parser.add_argument('-d', '--debug', action='store_true',
+                        help='Log debugging information')
+    args = parser.parse_args()
+
+    try:
+        config = yaml.load(open(args.config,'r').read())
+    except:
+        print 'Error: could not load the config file --> "%s"' % str(sys.exc_info()[1])
+        sys.exit(1)
+
+    main(args=args, config=config)
---
+++ readme.md
@@ -0,0 +1,91 @@
+# pyairbeam
+
+`pyairbeam` is a python library and script to interact with iOS/OSX devices running AirBeam ( http://appologics.com/airbeam ).
+
+## Why?
+
+AirBeam is an excellent and inexpensive app to easily use iOS/OSX devices to remote monitoring.
+However, I wanted to export 24x7 constant video streams form AirBeam devices to my own nas/s3 setup, using
+the recording device to do compression as well. Since AirBeam only allowed motion detection (which
+  I found to be unreliable), this module was written to interface with the REST interface AirBeam provides.
+
+## Whats it do?
+
+* Connects to the remote iOS/OSX device
+* Turns on the camera and starts recording
+* After specified time, rotates the file
+* File is compressed by the iOS/OSX device
+* After compression, remote file is downloaded and stored where specified
+* File storage folder is monitored to maintain a max file size; deleting the oldest files when necessary
+
+## Requirements
+
+In a virtualenv, `pip install -r requirements.txt`
+
+## Config
+
+Edit the `config.yaml`, shown below:
+
+```
+# Logfile Details
+log:
+    file: cam_slurper.log
+    format: '[%(asctime)s] [%(levelname)s] - %(message)s'
+    dateformat: '%Y-%m-%d %H:%M:%S'
+
+# Dynect Credentials; read-only required
+camera:
+    name: ipad2
+    hostname: http://blue.local
+    store_path: /Volumes/Surveillance/videos/
+
+# how often to rotate the files (in seconds) (900 is 15 minutes)
+rotate_duration: 900
+
+# maximum size (sum of files) in store_path, in MB. oldest files will be
+# deleted is size exceeds this value.
+max_dir_size: 50000
+```
+
+*log:file* - path and filename for the logfile.
+
+*log:format* - logging format. You can leave this as is.
+
+*log:dateformat* - datetime format. You can leav this as is.
+
+*camera:name* - a nickname for your remote device
+
+*camera:hostname* - http path to your device w/ AirBeam running.
+
+*camera:store_path* - location to store retrieved recorded files.
+
+*rotation_duration* - how often to create a new recording. Recommended between 15 - 60 minutes.
+Files are compressed on the remote device and large files will take up more space and take much longer to compress.
+
+*max_dir_size* - the maximum amount of space (in MB) you want the retrieved files to take up. When this size is
+surpassed, the oldest file(s) will be deleted.
+
+## Usage
+
+```
+usage: cam_slurper.py [-h] -c CONFIG [-d]
+
+Python Script to ensure an AirBeam device is recording and we are downloading
+the recordings periodically
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -c CONFIG, --config CONFIG
+                        (Required) YAML configuration file
+  -d, --debug           Log debugging information
+```
+
+Alternatively, import the `CamSlurper` object and write your script. The methods are self-explanatory.
+The HTTP server implemented by AirBeam has no protections - everything is just a simple GET request.
+Do not internet expose your devices via port-fowarding. If you wish to access this remotely, you should
+setup a VPN for your network, or push the recorded files to a remote location protected by auth (i.e., google drive, s3, etc).
+
+## TODOs / Issues
+
+* If AirBeam is shut off on the remote device while the script is running, the script will crash
+* Script only supports 1 device at the moment.

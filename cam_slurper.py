5b1c9722f1ef3d7953531f7ac0c98bb9f7ec7126
Chris Sandulow <chris@ares.home>
2014-09-17 12:58:40 -0400

    support multiple cameras and bug fix
    multiple cameras supported now; just add more cameras in the config file
    
    bug fix: if device is down when trying to connect; script will rety
    every 1 minute

--- cam_slurper.py
+++ cam_slurper.py
@@ -42,7 +42,7 @@ class CamSlurper:
 
     def __init__(self, name, hostname, store_path):
         self.hostname = hostname
-        self.name = name
+        self.name = name.replace(' ','_')
         self.storage_path = store_path
 
     def _rget(self, path):
@@ -52,7 +52,7 @@ class CamSlurper:
         if r.status_code == 200:
             return r
         else:
-            logging.error('Error getting %s: -> %s' % (url, str(sys.exc_info())))
+            logging.error('[%s] Error getting %s: -> %s' % (self.name, url, str(sys.exc_info())))
 
     def info(self):
         ''' Gets current info from recording device, returns JSON object of
@@ -71,11 +71,11 @@ class CamSlurper:
         ''' returns duration of recording, in number of seconds '''
         info = self.info()
         time = info['record_duration']
-        logging.debug('Raw Duration time: %s' % time)
+        logging.debug('[%s] Raw Duration time: %s' % (self.name,time))
         try:
             hours, mins, secs = time.split(':')
             duration = int(secs) + (int(mins) * 60) + (int(hours) * 3600)
-            logging.debug('Current Duration: %d' % duration)
+            logging.debug('[%s] Current Duration: %d' % (self.name,duration))
             return duration
         except:
             return 0
@@ -111,17 +111,17 @@ class CamSlurper:
             l = link.get('href')
             if l.startswith('/recording/'):
                 recordings.add(l.split('/')[-1])
-        logging.debug('Current files: %s' % str(recordings))
+        logging.debug('[%s] Current files: %s' % (self.name,str(recordings)))
         return recordings
 
     def download_recording(self, file_name):
         url = self.hostname + '/recording/' + file_name
-        path = self.storage_path + str(time.time())[:10] + '_' + file_name
+        path = self.storage_path + '/' + self.name + '_' + str(time.time())[:10] + '_' + file_name
         with open(path, 'wb') as handle:
             response = requests.get(url, stream=True)
 
             if not response.ok:
-                logging.error('Could not download/write file -> %s' % sys.exc_info())
+                logging.error('[%s] Could not download/write file -> %s' % (self.name, sys.exc_info()))
                 return 'not ok'
 
             for block in response.iter_content(1024):
@@ -129,13 +129,13 @@ class CamSlurper:
                     break
 
                 handle.write(block)
-        logging.info('Downloaded file %s; saved to %s' % (file_name, path))
+        logging.info('[%s] Downloaded file %s; saved to %s' % (self.name, file_name, path))
         return 'ok'
 
     def delete_recording(self, file_name):
         ''' Deletes a recording '''
         r = self._rget('delete/' + str(file_name))
-        logging.info('Device [%s]: "%s" deleted.' % (self.name, str(file_name)))
+        logging.info('[%s] "%s" deleted file on remote device.' % (self.name, str(file_name)))
 
     def get_storage_dir_size(self):
         ''' returns the size of the storage dir (in MB) '''
@@ -157,55 +157,75 @@ class CamSlurper:
         files = sorted(filel, reverse=True)
         delfile = self.storage_path + '/' + files[-1]
         os.remove(delfile)
-        logging.info('Deleted old file %s' % delfile)
+        logging.info('Exceeded directory size limit. Deleted old file %s' % delfile)
 
 def main(args, config):
     logging = setup_logging(config=config, args=args)
 
-    cname      = config['camera']['name']
-    hostname   = config['camera']['hostname']
-    store_path = config['camera']['store_path']
-    rotate     = config['rotate_duration']
-
     while True:
-        cs = CamSlurper(name=cname,
-                         hostname=hostname,
-                         store_path=store_path)
+        cameras = config['cameras']
+        rotate  = config['rotate_duration']
+        for camera in cameras:
+            cname      = camera['name']
+            hostname   = camera['hostname']
+            store_path = camera['store_path']
 
-        # Make sure Camera is OK to record
-        info = cs.info()
-        if info['camera_status'] != 'YES': cs.start_camera()
-        if info['record_avail'] != 'YES': cs.start_camera()
+            try:
+                cs = CamSlurper(name=cname,
+                                hostname=hostname,
+                                store_path=store_path)
 
-        # Check how long we've been recording for
-        duration = cs.get_duration()
-        # if we're not recording, start it.
-        if duration == 0:
-            logging.debug('Duration == 0')
-            cs.start_record()
-        # if we hit rotate time, rotate
-        elif duration > rotate:
-            logging.info('Rotating file')
-            cs.stop_record()
-            cs.start_record()
+                logging.debug('[%s] Init.' % cs.name)
 
-        # check the size of files in the storage directory
-        cur_size = cs.get_storage_dir_size()
-        while cur_size > int(config['max_dir_size']):
-            logging.debug('Current storage dir size is %d' % cur_size)
-            cs.remove_oldest_file()
-            time.sleep(1)
-            cur_size = cs.get_storage_dir_size()
+                # Make sure Camera is OK to record
+                info = cs.info()
+                if info['camera_status'] != 'YES': cs.start_camera()
+                if info['record_avail'] != 'YES': cs.start_camera()
 
-        # See if there are any files to download
-        files = cs.list_recordings()
-        if files:
-            for f in files:
-                resp = cs.download_recording(f)
-                if resp == 'ok':
-                    cs.delete_recording(f)
+                # Check how long we've been recording for
+                duration = cs.get_duration()
+                # if we're not recording, start it.
+                if duration == 0:
+                    logging.debug('Duration == 0')
+                    cs.start_record()
+                # if we hit rotate time, rotate
+                elif duration > rotate:
+                    logging.info('Rotating file')
+                    cs.stop_record()
+                    cs.start_record()
 
-        time.sleep(5)
+                # check the size of files in the storage directory
+                cur_size = cs.get_storage_dir_size()
+                while cur_size > int(config['max_dir_size']):
+                    logging.debug('Current storage dir size is %d' % cur_size)
+                    cs.remove_oldest_file()
+                    time.sleep(1)
+                    cur_size = cs.get_storage_dir_size()
+
+                # See if there are any files to download
+                files = cs.list_recordings()
+                if files:
+                    duration = cs.get_duration()
+                    for f in files:
+                        # if we're not recording, don't copy files (it will delay recording start)
+                        if duration == 0:
+                            continue
+                        resp = cs.download_recording(f)
+                        if resp == 'ok':
+                            cs.delete_recording(f)
+
+                time.sleep(5)
+                logging.debug('[%s] Sleeping' % cs.name)
+            except KeyboardInterrupt:
+                print 'Quitting'
+                sys.exit(0)
+            except requests.exceptions.ConnectionError:
+                logging.error('[%s] Problem Connecting to Camera: %s' % (cs.name, str(sys.exc_info())))
+                time.sleep(60)
+            except:
+                logging.error('[%s] Unhandled Exception: %s' % (cs.name, str(sys.exc_info())))
+                time.sleep(60)
+                continue
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='''Python Script to ensure an AirBeam
